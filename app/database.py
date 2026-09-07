4ba3123e31aade6f5a0e625b85884d62aadab890
Nikhil Sutar <nikhilsutar0601@gmail.com>
2026-09-07 22:44:33 +0530

    remove __pycache__ from git tracking

--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from urllib.parse import quote_plus
 from .config import settings
 
+# Safely encode special characters in the password
+encoded_password = quote_plus(settings.database_password)
 SQLALCHEMY_DATABASE_URL = f'postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
